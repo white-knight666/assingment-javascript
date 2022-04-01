c3ae2ba1e8d8874f54dd535c28cdfa6f1c931357
unknown <aw.sh2000@hotmail.com>
2022-04-02 00:55:34 +0200

    first commit

---
+++ first website with Framework/js.js
@@ -0,0 +1,17 @@
+document.getElementById("myBtn").addEventListener("click", displayDate);
+
+function displayDate() {
+  document.getElementById("demo").innerHTML = Date();
+}
+
+
+
+
+const cars = ["BMW", "Volvo", "Saab", "Ford", "Fiat", "Audi"];
+
+let text = "";
+for (let i = 0; i < cars.length; i++) {
+  text += cars[i] + "<br>";
+}
+
+document.getElementById("demo").innerHTML = text;
